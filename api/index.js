954377cb503df1cf3e9313f608958d8febe84af6
simo.zgaoua <m.zgaoua2001@gmail.com>
2023-10-05 19:51:51 +0100

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -26,3 +26,14 @@ app.get('/',(req,res) => {
 app.listen(3000 , ()=> {
     console.log("server is running on port 3000");
 })
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
